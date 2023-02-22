96a45ffeaa0a24961d855562614a2722a2111774
EdonaElshani <edonaelshani31@gmail.com>
2023-02-22 19:13:52 +0100

    fixed folder path

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
 import HeroSection from "./components/HomePage/HeroSection";
 import Navbar from "./components/HomePage/Navbar";
+import axios from 'axios';
 
 function App() {
+      useEffect(() => {
+        axios.get("http://localhost:8000/").then((response) => {
+          console.log(response.data);
+        });
+      }, []);
   return (
     <div>
       <Navbar />
