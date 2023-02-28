8fe770575f1ac3c1a3b368eb51c525a56edc4055
besnik420 <besniknurshaba@gmail.com>
2023-02-28 19:10:16 +0100

    jw token generated on login:

--- estate/app/Http/Controllers/api/AuthController.php
+++ estate/app/Http/Controllers/api/AuthController.php
@@ -8,18 +8,21 @@ use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+//use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller{
 
     public function Login(LoginRequest $request){
-        $creds = $request->validated();
-        if(!Auth::attempt($creds)){
-            return response(['message' =>'email or password is wrong']);
+        $credentials = $request->only('email', 'password');
+        
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
         }
-        /** @var User $user */
-        $user = Auth::user();
-        $token = $user->createToken('main')->plainTextToken;
-        return response(compact('user','token'));
+        
+        return response()->json(['token' => $token]);
+
     }
 
     public function Register(RegisterRequest $request){
